531b652e59dd1828bc6efbd4cd3757984ca9c3d2
Matt Foster <fmatthew40@gmail.com>
2022-01-27 21:07:51 -0700

    update get route in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,19 @@ const { restore } = require('../../models/Product');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll()
+  Product.findAll({
+    // attributes: ['id', ''],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
   .then(dbProductData => res.json(dbProductData))
   .catch(err => {
     console.log(err); 
