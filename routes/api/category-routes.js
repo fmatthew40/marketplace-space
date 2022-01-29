1b8c3386c33adad80a8408feb32e19c0eb0510a7
Matt Foster <fmatthew40@gmail.com>
2022-01-28 20:58:17 -0700

    delete, put added to category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,14 +50,49 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, 
+    // {
+    //   category_name: req.body.category 
+    //  },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'ID not found'});
+      return; 
+    }
+    res.json(dbCategoryData);
+  }) 
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 
